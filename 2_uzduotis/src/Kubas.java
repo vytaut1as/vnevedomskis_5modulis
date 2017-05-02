2a2dd865609c29f1f71a7ab608d3798262181c91
Vytautas <vytaut1as@gmail.com>
2017-05-02 11:11:36 +0300

    Antra uzduotis prideta

---
+++ 2_uzduotis/src/Kubas.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Kubas {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		double kub=0d;
+		double rut=0d;
+		double vand=0d;
+		double v=0d;
+		
+		System.out.println("Įveskite kubo kraštinės ilgį");
+		kub = sc.nextDouble();
+		System.out.println("Įveskite rutulio skersmenį");
+		rut = sc.nextDouble();
+		
+		if(rut>kub){
+			System.out.println("Sfera netelpa i kuba!");
+		}else{			
+
+			rut = (4d/3d)*Math.PI*Math.pow(rut/2, 3d);
+			kub = Math.pow(kub, 3d);
+
+			System.out.printf("Kubo tūris: %.2f %n", kub);
+			System.out.printf("Rutulio tūris: %.2f %n", rut);
+			System.out.printf("Vandens tūris: %.2f %n", kub-rut);
+			
+		}
+
+	}
+
+}
