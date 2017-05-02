a53ab3ac997b7d6862746cf9f69be95c44de0be0
Vytautas <vytaut1as@gmail.com>
2017-05-02 14:16:55 +0300

    ketvirta uzduotis patobulinta

--- 4_uzduotis/src/Kontrolinis.java
+++ 4_uzduotis/src/Kontrolinis.java
@@ -8,14 +8,22 @@ public class Kontrolinis {
 		int trejetas = 2; // gavo trejetus
 		int kiek=penketas*ketvertas*trejetas;
 		
-		while(kiek<=maxmok){
-		System.out.printf("Kontrolinio rezultatai: %n");
-		System.out.printf("Klasėje mokosi %d mokiniai %n", kiek);
-		System.out.printf("Klasėje %d mokiniai gavo penketus %n", kiek/penketas);
-		System.out.printf("Klasėje %d mokiniai gavo ketvertus %n", kiek/ketvertas);
-		System.out.printf("Klasėje %d mokiniai gavo trejetus %n", kiek/trejetas);
-		System.out.printf("Klasėje %d mokinių gavo nepatenkinamą pažymį %n", kiek-kiek/penketas-kiek/ketvertas-kiek/trejetas);
-		kiek+=kiek;
+		if(kiek>maxmok){
+			
+			System.out.println("Neatitinka salygos!");
+			
+		}else{
+			
+			while(kiek<=maxmok){
+			System.out.printf("Kontrolinio rezultatai: %n");
+			System.out.printf("Klasėje mokosi %d mokiniai %n", kiek);
+			System.out.printf("Klasėje %d mokiniai gavo penketus %n", kiek/penketas);
+			System.out.printf("Klasėje %d mokiniai gavo ketvertus %n", kiek/ketvertas);
+			System.out.printf("Klasėje %d mokiniai gavo trejetus %n", kiek/trejetas);
+			System.out.printf("Klasėje %d mokinių gavo nepatenkinamą pažymį %n", kiek-kiek/penketas-kiek/ketvertas-kiek/trejetas);
+			kiek+=kiek;
+			}
+			
 		}
 
 		
