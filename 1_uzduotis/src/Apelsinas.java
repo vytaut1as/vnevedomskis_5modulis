8ff67df990b401532dc52d226cecbd6ec1a24b42
Vytautas <vytaut1as@gmail.com>
2017-05-02 10:49:49 +0300

    Pirma uzduotis ispresta

---
+++ 1_uzduotis/src/Apelsinas.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Apelsinas {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		double d=0d;
+		double a=0d;
+		double v=0d;
+		
+		System.out.println("įveskite apelsino skersmenį: ");
+		d = sc.nextDouble();
+		System.out.println("įveskite apelsino žievelės storį: ");
+		a =sc.nextDouble();
+		v = (4d/3d)*Math.PI*Math.pow(d/2-a, 3d);
+		System.out.printf("Apelsino minkštimo tūris: %.2f", v);
+	}
+
+}
