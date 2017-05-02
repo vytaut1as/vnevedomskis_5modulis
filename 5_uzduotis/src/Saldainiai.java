c3e4b2545f0564d6ca781a3bd6243d42c3e32601
Vytautas <vytaut1as@gmail.com>
2017-05-02 14:57:15 +0300

    Penkta uzduotis, tikrai saziningai skaiciuoja

---
+++ 5_uzduotis/src/Saldainiai.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Saldainiai {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int kaina =0;
+		int pinigu=0;
+		int kupon=0;
+		int kiek=0;
+		
+		System.out.println("Įveskite saldainių maišelio kainą: ");
+		kaina= sc.nextInt();
+		System.out.println("Įveskite pinigų sumą skirtą saldainiams pirkti: ");
+		pinigu = sc.nextInt();
+		
+		while(pinigu>=kaina){
+			
+			pinigu-=kaina;
+			++kiek;
+			++kupon;
+			if(kupon==3){
+				kupon=1;
+				++kiek;
+			}
+			
+		}
+		
+		System.out.printf("Saldainių maišeliu: %d\n", kiek);
+//		vienas po 1 yra 17
+//		17 maiseliu 17 kuponu
+//		17/3=5, 17+5=22 maiseliai ir 5+2=7 kuponai
+//		7/3, 22+2=24 maiseliai ir 2+1=3 kuponai
+//		3/3, 24+1=25 maiseliai ir 1 kuponas
+
+	}
+
+}
